bfdab272e04a768ca9b9fc762805489db7153a7b
ingridmicaa1 <gualanmicaela71@gmail.com>
2026-06-22 17:43:26 -0500

    Funcionalidad3

--- EJERCICIO1/APEB2_Taller11/Solucion_Codigo/JuegoRoles/src/juegoroles/Personajes.java
+++ EJERCICIO1/APEB2_Taller11/Solucion_Codigo/JuegoRoles/src/juegoroles/Personajes.java
@@ -11,6 +11,10 @@ public abstract class Personajes {
     protected int vida;
     protected int experiencia;
     protected String estado;
+    protected int energia;
+    protected int cooldown;
+    protected int cooldownMax;
+    
     
     
     public abstract void atacar(int vida);
@@ -21,7 +25,35 @@ public abstract class Personajes {
         this.vida = vida;
         this.experiencia = experiencia;
         this.estado = "";
+        this.energia = 100;
+        this.cooldown = 0;
+        this.cooldownMax = 3;
+
     }
+    public void usarHabilidadEspecial() {
+        
+        if (energia < 20) {
+            System.out.println("No hay suficiente energía ");
+            return;
+        }
+        
+        if (cooldown > 0) {
+            System.out.println("Habilidad en cooldown: " + cooldown);
+            return;
+        }
+
+        System.out.println("Usa habilidad especial ");
+
+        energia = energia - 20;
+        cooldown = cooldownMax;
+    }
+     
+      public void reducirCooldown() {
+        if (cooldown > 0) {
+            cooldown--;
+        }
+    }
+
 
     public int getVida() {
         return vida;
